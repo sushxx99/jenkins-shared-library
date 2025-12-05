13e40b16dcee37b0ab538eba69910ac71739de6a
sushxx99 <sush9903@gmail.com>
2025-12-05 11:56:41 +0000

    Added dynamic port mapping for all microservices

--- vars/deployPipeline.groovy
+++ vars/deployPipeline.groovy
@@ -1,5 +1,16 @@
 def call() {
+
     echo "Running CD Pipeline for ${env.JOB_NAME}"
+
+    // Define port mapping based on microservice name
+    def port = 8080
+    if (env.JOB_NAME == "jenkins-demo-app") {
+        port = 8081
+    } else if (env.JOB_NAME == "jenkins-demo-app-2") {
+        port = 8082
+    } else if (env.JOB_NAME == "jenkins-demo-app-3") {
+        port = 8083
+    }
 
     stage('Container Build') {
         sh """
@@ -11,7 +22,7 @@ def call() {
         sh """
         sudo podman stop ${env.JOB_NAME} || true
         sudo podman rm ${env.JOB_NAME} || true
-        sudo podman run -d --name ${env.JOB_NAME} -p 8080:8080 ${env.JOB_NAME}:latest
+        sudo podman run -d --name ${env.JOB_NAME} -p ${port}:8080 ${env.JOB_NAME}:latest
         """
     }
 }
